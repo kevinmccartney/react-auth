aa13a4f650cab32f0a59112677726651df6c331f
Kevin McCartney <hello@kevinmccartney.is>
2017-03-10 14:05:16 -0500

    Now there's Redux woooo

--- src/index.js
+++ src/index.js
@@ -1,8 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { compose, createStore, applyMiddleware } from 'redux'
+import { composeWithDevTools } from 'redux-devtools-extension';
+import createLogger from 'redux-logger';
+import { Provider } from 'react-redux'
+
+import rootReducer from './reducers';
+
 import AppWrapper from './containers/AppWrapper';
 
+const enhancer = compose(
+  composeWithDevTools(),
+  applyMiddleware(createLogger())
+);
+
+const store = createStore(rootReducer, enhancer);
+
 ReactDOM.render(
-  <AppWrapper />,
+  <Provider store={store}>
+    <AppWrapper />
+  </Provider>,
   document.getElementById('root')
 );
